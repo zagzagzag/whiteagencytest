be58ccabc4b3b96642d18b3aaedcd1f64e273713
zagzagzag <bastien.c@live.com>
2014-09-13 19:46:27 +1000

    add assignement.js

---
+++ js/assignment.js
@@ -0,0 +1,25 @@
+﻿(function ($, whiteAgency) {
+
+
+
+
+
+
+
+
+}(jQuery, window.whiteAgencyAssignment = window.whiteAgencyAssignment || {}));
+
+
+
+function getData() {
+    $.getJSON("js/data.json", function (data) {
+        console.log(data);
+        console.log("Data: " + data);
+    });
+
+}
+
+//function getDataSuccess(data, status) {
+//        console.log(data);
+//        console.log(status);
+//}
---
+++ js/jquery.lightbox.js
@@ -0,0 +1,38 @@
+﻿
+/*!
+ * jQuery Lightbox plugin
+ * Requires jQuery v1.5 or later
+ * Author: Bastien Cabirou
+ */
+
+(function ($, window) {
+
+    //$.Lightbox = function (elements, options) {
+    //    var Lightbox = this;
+        
+    //    Lightbox.init = function () {
+    //        console.log('init');
+    //        var _this = this;
+    //        $('lightbox-controller').click(function () {
+    //            _this.Lightbox($(this).attr('lightbox-action'), $(this.attr('lightbox-id')));
+    //        });
+    //    };
+
+    //    Lightbox.init();
+    //};
+
+    $.fn.Lightbox = function (action, source) {
+        if ( action === "open") {
+            var screenHeight = $(window).height();
+            var lightboxHeight = $('#lightbox-loading .box').height();
+            $('#lightbox-loading .box').css('top', (screenHeight / 2) - (lightboxHeight / 2));
+            $('#lightbox-loading').show();
+        }
+ 
+        if ( action === "close" ) {
+            $('#lightbox-loading').hide();
+        }
+
+    };
+
+}(jQuery, window));
